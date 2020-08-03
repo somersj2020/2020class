8fb63bfef3c496bfb3b243d7a8a1a6e35b81dada
Jeff Somers <somersj@amazon.com>
2020-08-03 15:17:29 -0400

    Change math logic to smaller number

--- Test/src/Test.java
+++ Test/src/Test.java
@@ -2,7 +2,7 @@
 class Test {
     public static void main(String[] args) {
         int value = 2;
-        int exponent = 16;
+        int exponent = 2;
         double result = Math.pow(value, exponent);
         System.out.println("result is: " + result);
     }
