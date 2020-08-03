4ced3134cf1cd9be0922be8226dd04786984e93d
Jeff Somers <somersj@amazon.com>
2020-08-03 15:12:19 -0400

    Add math logic

---
+++ Test/src/Test.java
@@ -0,0 +1,9 @@
+
+class Test {
+    public static void main(String[] args) {
+        int value = 2;
+        int exponent = 16;
+        double result = Math.pow(value, exponent);
+        System.out.println("result is: " + result);
+    }
+}
